f645275d143abe6dadef7c5a19cbd9fd6db7f17b
Mark Rofail <markm.rofail@gmail.com>
2019-12-07 15:25:15 +0200

    fix: generify menuItem methods

--- controller/shops/hm/categories.go
+++ controller/shops/hm/categories.go
@@ -7,6 +7,24 @@ import (
 	"net/http"
 )
 
+var numericAlpha = map[int]string{
+	1:  "one",
+	2:  "two",
+	3:  "three",
+	4:  "four",
+	5:  "five",
+	6:  "six",
+	7:  "seven",
+	8:  "eight",
+	9:  "nine",
+	10: "ten",
+}
+
+type MenuItem struct {
+	Title string
+	Body  *goquery.Selection
+}
+
 func GetAllTitles() {
 	resp, err := http.Get("https://eg.hm.com/en/")
 	if err != nil {
@@ -21,48 +39,74 @@ func GetAllTitles() {
 	idMainMenu := ".menu__list.menu--one__list"
 	mainList := doc.Find("ul" + idMainMenu)
 
-	getMenu1Items(mainList)
+	nextMenuKeywords := []string{"Men", "Ladies", "Kids"}
+	getMenu1Items(mainList, nextMenuKeywords)
 }
 
-func getMenu1Items(item *goquery.Selection) {
-	idMenu := fmt.Sprintf(".menu__list-item.menu--%s__list-item", "one")
+func getMenu1Items(item *goquery.Selection, keywords []string) {
+	idMenu := fmt.Sprintf(".menu__list-item.menu--%s__list-item", numericAlpha[1])
 
 	item.Find("li" + idMenu).Each(func(index int, menu1 *goquery.Selection) {
-		idMenuItemText := fmt.Sprintf(".menu__link.menu--%s__link", "one")
-		idMenuItemBody := fmt.Sprintf(".menu__list.menu--%s__list", "two")
+		idMenuItemText := fmt.Sprintf(".menu__link.menu--%s__link", numericAlpha[1])
+		idMenuItemBody := fmt.Sprintf(".menu__list.menu--%s__list", numericAlpha[2])
 
 		title := menu1.Find(idMenuItemText).Text()
-		fmt.Println(title)
-
 		body := menu1.Find(idMenuItemBody)
-		getMenu2Items(body, "Shop By Product")
+
+		menuItem := MenuItem{
+			Title: title,
+			Body:  body,
+		}
+
+		fmt.Println(menuItem.Title)
+		nextMenuKeywords := []string{"Shop By Product"}
+		if len(keywords) > 0 {
+			for _, keyword := range keywords {
+				if menuItem.Title == keyword {
+					getMenu2Items(menuItem.Body, nextMenuKeywords)
+				}
+			}
+		} else {
+			getMenu2Items(menuItem.Body, nextMenuKeywords)
+		}
 	})
 }
 
-func getMenu2Items(item *goquery.Selection, keyword string) {
-	idMenu := fmt.Sprintf(".menu__list-item.menu--%s__list-item", "two")
+func getMenu2Items(item *goquery.Selection, keywords []string) {
+	idMenu := fmt.Sprintf(".menu__list-item.menu--%s__list-item", numericAlpha[2])
 
 	item.Find("li" + idMenu).Each(func(index int, menu2 *goquery.Selection) {
-		idMenuItemText := fmt.Sprintf(".menu__link.menu-%s__link", "two")
-		idMenuItemBody := fmt.Sprintf(".menu__list.menu--%s__list", "three")
+		idMenuItemText := fmt.Sprintf(".menu__link.menu-%s__link", numericAlpha[2])
+		idMenuItemBody := fmt.Sprintf(".menu__list.menu--%s__list", numericAlpha[3])
 
 		title := menu2.Find(idMenuItemText).Text()
-		fmt.Println("> " + title)
-
 		body := menu2.Find(idMenuItemBody)
-		if title == keyword{
-			getMenu3Items(body)
+
+		menuItem := MenuItem{
+			Title: title,
+			Body:  body,
+		}
+
+		fmt.Println("> " + menuItem.Title)
+		if len(keywords) > 0 {
+			for _, keyword := range keywords {
+				if menuItem.Title == keyword {
+					getMenu3Items(menuItem.Body)
+				}
+			}
+		} else {
+			getMenu3Items(menuItem.Body)
 		}
 	})
 }
 
 func getMenu3Items(item *goquery.Selection) {
-	idMenu := fmt.Sprintf(".menu__list-item.menu--%s__list-item", "three")
+	idMenu := fmt.Sprintf(".menu__list-item.menu--%s__list-item", numericAlpha[3])
 
 	item.Find("li" + idMenu).Each(func(i int, menuItem *goquery.Selection) {
-		idMenuItemText := fmt.Sprintf(".menu__link.menu--%s__link", "three")
+		idMenuItemText := fmt.Sprintf(".menu__link.menu--%s__link", numericAlpha[3])
 
 		title := menuItem.Find(idMenuItemText).Text()
-		fmt.Println("> > " + title)
+		fmt.Println(" > " + title)
 	})
 }
